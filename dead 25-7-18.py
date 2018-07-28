6b4cc5128e38743159a40d59be0cd92166e36896
erez-code <41819488+erez-code@users.noreply.github.com>
2018-07-28 11:15:08 +0300

    Add files via upload

---
+++ dead 25-7-18.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+def dead(a,b):
+    if a == "female":
+        return b+87
+    elif a == "male":
+        return b+85
+    else:
+        print("Unknown gender input...")
+        return b+86
+a = input("Are you Male or Female? ").lower()
+b = int(input("I was born in the year: "))
+print("You'll probably die around the year", dead(a,b))
+input("I was born in the year: ")
+
